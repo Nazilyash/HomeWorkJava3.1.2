16487c76837573c38d2b6f13839c90a54855beac
misharka <fareshta88@gmail.com>
2021-03-17 22:34:26 +0300

    Radio.java modified

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,22 +1,64 @@
 package ru.netology.domain;
 
 public class Radio {
+    private int stationQuantity=10;
     private String name;
     private int maxStationNumber;
-    private int minStationNumber;
+    private int minStationNumber=0;
     private int currentStationNumber;
-    private int maxVolume;
-    private int minVolume;
+    private int maxVolume=100;
+    private int minVolume=0;
     private int currentVolume;
-    private boolean on;
+    private boolean on=true;
 
-    public String getName() {
-        return name;
+    public Radio() {
     }
+
+    public Radio(String name, int stationQuantity, int maxStationNumber, int minStationNumber, int currentStationNumber, int maxVolume, int minVolume, int currentVolume, boolean on) {
+        this.name = name;
+        this.stationQuantity = stationQuantity;
+        this.maxStationNumber = maxStationNumber;
+        this.minStationNumber = minStationNumber;
+        this.currentStationNumber = currentStationNumber;
+        this.maxVolume = maxVolume;
+        this.minVolume = minVolume;
+        this.currentVolume = currentVolume;
+        this.on = on;
+    }
+
+    public Radio(int stationQuantity, String name) {
+
+        this.stationQuantity = stationQuantity;
+        this.name = name;
+    }
+
+    public Radio(int stationQuantity, int currentStationNumber) {
+        this.stationQuantity = stationQuantity;
+        maxStationNumber=stationQuantity-1;
+        if (currentStationNumber > maxStationNumber) {
+            this.currentStationNumber = minStationNumber;
+            return;
+        }
+        this.currentStationNumber = currentStationNumber;
+    }
+
+    public Radio(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            this.currentVolume = maxVolume;
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public String getName() {return name;}
 
     public void setName(String name) {
         this.name = name;
     }
+
+    public int getStationQuantity() {return stationQuantity;}
+
+    public void setStationQuantity(int stationQuantity) {this.stationQuantity = stationQuantity;}
 
     public void setMaxStationNumber(int maxStationNumber) {
         this.maxStationNumber = maxStationNumber;
@@ -30,13 +72,7 @@ public class Radio {
         return currentStationNumber;
     }
 
-    public void setCurrentStationNumber(int currentStationNumber) {
-        if (currentStationNumber > maxStationNumber) {
-            this.currentStationNumber = minStationNumber;
-            return;
-        }
-        this.currentStationNumber = currentStationNumber;
-    }
+    public void setCurrentStationNumber(int currentStationNumber) {this.currentStationNumber = currentStationNumber; }
 
     public void increaseCurrentStationNumber() {
         if (currentStationNumber < maxStationNumber) {
@@ -67,10 +103,6 @@ public class Radio {
     }
 
     public void setCurrentVolume(int currentVolume) {
-        if (currentVolume > maxVolume) {
-            this.currentVolume = maxVolume;
-            return;
-        }
         this.currentVolume = currentVolume;
     }
 
