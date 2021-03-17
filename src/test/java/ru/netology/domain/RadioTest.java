e08f1b14d74657b3f0a5e9ace97aa85d95cdc60e
misharka <fareshta88@gmail.com>
2021-03-17 22:32:41 +0300

    pom.xml and RadioTest.java modified

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -7,61 +7,53 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
-    @Test
-    public void shouldGetAndSetName() {
-        Radio radio = new Radio();
-        String expected = "Радио";
-        assertNull(radio.getName());
-        radio.setName(expected);
-        assertEquals(expected, radio.getName());
+    @ParameterizedTest
+    @CsvSource(
+            value = {"5, 'Малое'",
+                    "100, 'Большое'",
+                    "1000, 'Гигантское'"
+            }
+    )
+    public void shouldSetNameAndStationQuantity(int stationQuantity, String name) {
+        Radio radio = new Radio(stationQuantity,name);
+        assertEquals(stationQuantity, radio.getStationQuantity());
+        assertEquals(name, radio.getName());
     }
 
     @ParameterizedTest
     @CsvSource(
-            value = {"5, 5",
-                    "0, 0",
-                    "0, 15"
+            value = {"5, 5, 10",
+                    "0, 0, 100",
+                    "0, 15, 3"
             }
     )
-    public void shouldSetCurrentStationNumber(int expectedCurrentStationNumber, int currentStationNumber) {
-        Radio radio = new Radio();
-        radio.setOn(true);
-        radio.setMaxStationNumber(9);
-        radio.setMinStationNumber(0);
-        radio.setCurrentStationNumber(currentStationNumber);
+    public void shouldSetCurrentStationNumber(int expectedCurrentStationNumber, int currentStationNumber, int stationQuantity) {
+        Radio radio = new Radio(stationQuantity, currentStationNumber);
         assertEquals(expectedCurrentStationNumber, radio.getCurrentStationNumber());
     }
 
     @ParameterizedTest
     @CsvSource(
-            value = {"6, 5",
-                    "0, 9",
-                    "1, 10"
+            value = {"6, 5, 50",
+                    "0, 4, 5",
+                    "1, 10, 5"
             }
     )
-    public void shouldIncreaseCurrentStationNumber(int expectedCurrentStationNumber, int currentStationNumber) {
-        Radio radio = new Radio();
-        radio.setOn(true);
-        radio.setMaxStationNumber(9);
-        radio.setMinStationNumber(0);
-        radio.setCurrentStationNumber(currentStationNumber);
+    public void shouldIncreaseCurrentStationNumber(int expectedCurrentStationNumber, int currentStationNumber, int stationQuantity) {
+        Radio radio = new Radio(stationQuantity, currentStationNumber);
         radio.increaseCurrentStationNumber();
         assertEquals(expectedCurrentStationNumber, radio.getCurrentStationNumber());
     }
 
     @ParameterizedTest
     @CsvSource(
-            value = {"4, 5",
-                    "9, 0",
-                    "9, 11"
+            value = {"4, 5, 20",
+                    "1, 0, 2",
+                    "59, 60, 60"
             }
     )
-    public void shouldDecreaseCurrentStationNumber(int expectedCurrentStationNumber, int currentStationNumber) {
-        Radio radio = new Radio();
-        radio.setOn(true);
-        radio.setMaxStationNumber(9);
-        radio.setMinStationNumber(0);
-        radio.setCurrentStationNumber(currentStationNumber);
+    public void shouldDecreaseCurrentStationNumber(int expectedCurrentStationNumber, int currentStationNumber, int stationQuantity) {
+        Radio radio = new Radio(stationQuantity, currentStationNumber);
         radio.decreaseCurrentStationNumber();
         assertEquals(expectedCurrentStationNumber, radio.getCurrentStationNumber());
     }
@@ -69,16 +61,12 @@ class RadioTest {
     @ParameterizedTest
     @CsvSource(
             value = {"6, 5",
-                    "10, 10",
-                    "10, 20"
+                    "100, 100",
+                    "100, 101"
             }
     )
     public void shouldIncreaseVolume(int expectedCurrentVolume, int currentVolume) {
-        Radio radio = new Radio();
-        radio.setOn(true);
-        radio.setMaxVolume(10);
-        radio.setMinVolume(0);
-        radio.setCurrentVolume(currentVolume);
+        Radio radio = new Radio(currentVolume);
         radio.increaseCurrentVolume();
         assertEquals(expectedCurrentVolume, radio.getCurrentVolume());
     }
@@ -87,15 +75,11 @@ class RadioTest {
     @CsvSource(
             value = {"4, 5",
                     "0, 0",
-                    "9, 11"
+                    "99, 101"
             }
     )
     public void shouldDecreaseCurrentVolume(int expectedCurrentVolume, int currentVolume) {
-        Radio radio = new Radio();
-        radio.setOn(true);
-        radio.setMaxVolume(10);
-        radio.setMinVolume(0);
-        radio.setCurrentVolume(currentVolume);
+        Radio radio = new Radio(currentVolume);
         radio.decreaseCurrentVolume();
         assertEquals(expectedCurrentVolume, radio.getCurrentVolume());
     }
